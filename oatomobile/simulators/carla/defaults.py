734ea7b4adee3023db306d1737c8ba587da9c778
chenbq1234 <chenbq6@gmail.com>
2021-05-31 00:06:55 -0400

    reduced replan step to 5

--- oatomobile/simulators/carla/defaults.py
+++ oatomobile/simulators/carla/defaults.py
@@ -306,7 +306,7 @@ LIDAR_SENSOR_CONFIG = {
 GOAL_SENSOR_CONFIG = {
     "num_goals": 20,
     "sampling_radius": 2.0,
-    "replan_every_steps": 10,
+    "replan_every_steps": 5,
 }
 
 # The game state configuration.
