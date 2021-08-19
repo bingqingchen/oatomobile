a3bc1fa0745cd10655f67be18faf2072fa2d63bb
chenbq1234 <chenbq6@gmail.com>
2021-08-19 15:28:01 -0400

    included Return and Distance in Metrics

--- oatomobile/benchmarks/__init__.py
+++ oatomobile/benchmarks/__init__.py
@@ -15,6 +15,8 @@
 """Public API for `oatomobile.benchmarks`."""
 
 from oatomobile.benchmarks.carnovel.benchmark import carnovel
-from oatomobile.benchmarks.corl2017.benchmark import original
-from oatomobile.benchmarks.nocrash.benchmark import nocrash
+#from oatomobile.benchmarks.corl2017.benchmark import original
+#from oatomobile.benchmarks.nocrash.benchmark import nocrash
 from oatomobile.benchmarks.anyweather.benchmark import anyweather
+from oatomobile.benchmarks.corl2017_new_town_suite.benchmark import original, corl2017
+from oatomobile.benchmarks.nocrash_new_town_suite.benchmark import nocrash
